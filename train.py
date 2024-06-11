48c79616d1db4f9f7f1eb282bc2622697eed3243
Ryan <ryan@geisen.us>
2024-06-11 16:39:04 +0000

    just current training parameters

--- train.py
+++ train.py
@@ -11,40 +11,49 @@ from torch.utils.data import random_split
 from dataset_class import Custom_Traffic_Dataset
 from vivit import ViVit
 
+from train_utils import evaluate_model, print_metrics, save_checkpoint, load_checkpoint
+
 def main():
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     
     # Model Parameters
     size = 512              # Height and width of frame
     tube_hw = 16            # height and width of tubelet, frame size must be divisible by this number
-    tube_d = 4              # depth of tubelet, i.e. number of frames back 
+    tube_d = 16              # depth of tubelet, i.e. number of frames back 
     n_channels = 3          # R,G,B
     latent_size = 768       # Size of embedding, 
     num_class = 2           # Num of classes
-    num_heads = 12          # Number of attention heads in a single encoder
-    num_encoders = 12       # Number of encoders in model
+    num_heads = 24          # Number of attention heads in a single encoder
+    num_encoders = 24       # Number of encoders in model
 
     #Training Parameters
     dropout = 0.1           # Dropout
     epochs = 10             # Number of iterations through entirety of Data
-    base_lr = 3e-3         # learning rate
+    base_lr = 3e-3          # learning rate
     weight_decay = 0.03     # Weight Decay
-    batch_size = 26         # batch size
+    batch_size = 12         # batch size
     loss_steps = 10
     val_steps = 200
+    
+    model_num = 3
+    load_from_checkpoint = False
+    mean= [0.41433686 0.41618344 0.4186155 ], 
+    std= [0.1953987  0.19625916 0.19745596]
+    checkpoint_path = f'./model{model_num}/checkpoint.pth'
+    best_checkpoint_path = f'./model{model_num}/best_checkpoint.pth'
     
     print('Loading Model...')
     model = ViVit(num_encoders, latent_size, device, num_heads, num_class, dropout, tube_hw, tube_d, n_channels, batch_size)
     
     print('Loading Dataset...')
-    train_data = Custom_Traffic_Dataset(tube_d, size, n_channels, "/data/ryan_thesis_ds/thesis_videos", '/data/ryan_thesis_ds/crash_frame_numbers.txt')
+    train_data = Custom_Traffic_Dataset(tube_d, size, n_channels, "/data/ryan_thesis_ds/thesis_videos", '/data/ryan_thesis_ds/crash_frame_numbers.txt', mean, std)
     generator1 = torch.Generator().manual_seed(42)      # Set seed so train/test/validate splits remain the same throughout all experiments
     train_data, test_data, val_data = random_split(train_data, [0.64, 0.2, 0.16], generator=generator1)
     datasets = {'train': train_data, 'test': test_data, 'val': val_data}
     
     print('Initializing Dataloader...')
     dataloaders = {x: torch.utils.data.DataLoader(datasets[x], batch_size=batch_size,
-                                            shuffle=True, num_workers=8, drop_last = True) for x in ['train','test', 'val']}
+                                            shuffle=True, num_workers=4, drop_last = True) for x in ['train','test', 'val']}
 
     print('Setting up training parameters...')
     # Betas used for Adam in paper are 0.9 and 0.999, which are the default in PyTorch
@@ -101,56 +110,18 @@ def main():
 
         # Save the figure to a file
         plt.draw()
-        plt.savefig('./plot_test2.png')
-        
-
-    #Eval function for readability 
-    def evaluate_model(model, dataloader, criterion, device):
-        model.eval()
-        running_loss = 0
-        running_corrects = 0
-        TP=TN=FP=FN=0
-
-        with torch.no_grad():
-            for inputs, targets in tqdm(dataloader, desc='Evaluation Loop'):
-                inputs, targets = inputs.to(device), targets.to(device)
-                outputs = model(inputs)
-                _, preds = torch.max(outputs, 1)
-                loss = criterion(outputs, targets)
-                running_loss += loss.item()
-                running_corrects += torch.sum(preds == targets.data)
-                # Calculate TP, TN, FP, FN
-                TP += ((preds == 1) & (targets == 1)).sum().item()
-                TN += ((preds == 0) & (targets == 0)).sum().item()
-                FP += ((preds == 1) & (targets == 0)).sum().item()
-                FN += ((preds == 0) & (targets == 1)).sum().item()
-        
-        avg_loss = running_loss / len(dataloader)
-        accuracy = running_corrects.double() / len(dataloader.dataset)
-        precision = TP / (TP + FP) if (TP + FP) > 0 else 0
-        recall = TP / (TP + FN) if (TP + FN) > 0 else 0
-
-        return avg_loss, accuracy, precision, recall
-    
-    
-    #Print Metrics function for readability
-    def print_metrics(epoch, batch_idx, loss_avg, acc, precision, recall, phase='Training'):
-        if phase == 'Validation':
-            print(f'{phase} - Epoch: {epoch + 1} Batch: {batch_idx +1} Loss: {loss_avg:.4f} Accuracy: {acc:.4f} Precision: {precision:.4f} Recall: {recall:.4f}')
-        else:
-            print(f'{phase} - Epoch: {epoch + 1} Batch: {batch_idx +1} Loss: {loss_avg:.4f} Accuracy: {acc:.4f}')
-
-
-    def save_checkpoint(state, filename):
-        directory = os.path.dirname(filename)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-            
-        torch.save(state, filename)
+        plt.savefig(f'./plots/plot_test{model_num}.png')
         
         
     # Training Loop
     def train_model(model, dataloaders, datasets, criterion, optimizer, scheduler, device, epochs, loss_steps, val_steps, batch_size):
+        best_val_recall = 0.0
+        start_epoch = 0
+        start_batch_idx = 0
+        
+        if load_from_checkpoint:
+            start_epoch, _, start_batch_idx = load_checkpoint(model, optimizer, scheduler, checkpoint_path, device)
+             
         model.train().to(device)
         metrics = {
             'train_losses': [],
@@ -160,16 +131,16 @@ def main():
             'val_precisions': [],
             'val_recalls': []
         }
-        best_val_recall = 0.0
-        checkpoint_path = './model2/checkpoint.pth'
-        best_checkpoint_path = './model2/best_checkpoint.pth'
         
-
+        
         print(f'Starting Training on Device: {device}...')
-        for epoch in tqdm(range(epochs), total=epochs):
+        for epoch in tqdm(range(start_epoch, epochs), total=epochs):
             running_loss = 0.0
             running_corrects = 0
+            
             for batch_idx, (inputs, targets) in enumerate(tqdm(dataloaders['train'], desc=f'Training Loop {epoch}')):
+                if epoch == start_epoch and batch_idx < start_batch_idx:
+                    continue  # Skip batches processed in the previous run
                 inputs, targets = inputs.to(device), targets.to(device)
                 
                 optimizer.zero_grad() # reset gradients for proper backwards pass 
@@ -187,7 +158,7 @@ def main():
                 running_corrects += torch.sum(preds == targets.data)
                 
                 if (batch_idx+1)%loss_steps == 0:
-                    step = len(datasets['train'])*epoch + batch_idx
+                    step = len(dataloaders['train'])*epoch + batch_idx
                     acc = running_corrects.double()/(loss_steps*batch_size)
                     loss_avg = running_loss/loss_steps
                     print_metrics(epoch, batch_idx, loss_avg, acc, None, None)
@@ -204,7 +175,7 @@ def main():
                 if (batch_idx+1)%val_steps == 0:
                     print('Starting Validation...')
                     val_loss, val_acc, val_precision, val_recall = evaluate_model(model, dataloaders['val'], criterion, device)
-                    step = len(datasets['train']) * epoch + batch_idx
+                    step = len(dataloaders['train']) * epoch + batch_idx
 
                     metrics['val_losses'].append((step, val_loss))
                     metrics['val_accs'].append((step, val_acc))
@@ -220,11 +191,12 @@ def main():
                     'optimizer_state_dict': optimizer.state_dict(),
                     'scheduler_state_dict': scheduler.state_dict(),
                     'loss': val_loss,
+                    'batch_idx': batch_idx
                     }
                     
                     save_checkpoint(checkpoint, checkpoint_path)
                     # Save the best checkpoint based on validation recall
-                    if val_recall > best_val_recall:
+                    if val_recall > best_val_recall and val_recall != 1.0:
                         best_val_recall = val_recall
                         save_checkpoint(checkpoint, best_checkpoint_path)
                     
